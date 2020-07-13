ff211316a0ccee63d1cf49eadbae5c906508517f
Piotr Jastrzebski <haaawk@gmail.com>
2020-07-13 19:29:35 +0200

    replicator: Handle collections
    Make replicator work with updates to sets, maps and lists.

--- scylla-cdc-replicator/src/main/java/com/scylladb/scylla/cdc/replicator/Main.java
+++ scylla-cdc-replicator/src/main/java/com/scylladb/scylla/cdc/replicator/Main.java
@@ -1,7 +1,10 @@
 package com.scylladb.scylla.cdc.replicator;
 
+import static com.datastax.driver.core.querybuilder.QueryBuilder.addAll;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.bindMarker;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
+import static com.datastax.driver.core.querybuilder.QueryBuilder.putAll;
+import static com.datastax.driver.core.querybuilder.QueryBuilder.removeAll;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.set;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.timestamp;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.ttl;
@@ -9,6 +12,7 @@ import static com.datastax.driver.core.querybuilder.QueryBuilder.ttl;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -17,16 +21,24 @@ import java.util.stream.Collectors;
 
 import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.CodecRegistry;
 import com.datastax.driver.core.ColumnDefinitions.Definition;
 import com.datastax.driver.core.ColumnMetadata;
 import com.datastax.driver.core.ConsistencyLevel;
+import com.datastax.driver.core.DataType;
 import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.RegularStatement;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.Statement;
 import com.datastax.driver.core.TableMetadata;
+import com.datastax.driver.core.TypeCodec;
+import com.datastax.driver.core.querybuilder.Assignment;
 import com.datastax.driver.core.querybuilder.Delete;
 import com.datastax.driver.core.querybuilder.Insert;
+import com.datastax.driver.core.querybuilder.ListSetIdxTimeUUIDAssignment;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.datastax.driver.core.querybuilder.Update;
+import com.google.common.reflect.TypeToken;
 import com.scylladb.cdc.Change;
 import com.scylladb.cdc.ChangeConsumer;
 import com.scylladb.cdc.ScyllaCDC;
@@ -44,30 +56,26 @@ public class Main {
     private static final String TIMESTAMP_MARKER_NAME = "using_timestamp_bind_marker";
     private static final String TTL_MARKER_NAME = "using_ttl_bind_marker";
 
-    private static abstract class PreparedOperation {
+    private static interface Operation {
+      Statement getStatement(Change c, ConsistencyLevel cl);
+    }
+
+    private static long timeuuidToTimestamp(UUID from) {
+      return (from.timestamp() - 0x01b21dd213814000L) / 10;
+    }
+
+    private static abstract class PreparedOperation implements Operation {
       protected final TableMetadata table;
       protected final PreparedStatement preparedStmt;
 
-      protected static long timeuuidToTimestamp(UUID from) {
-        return (from.timestamp() - 0x01b21dd213814000L) / 10;
+      protected abstract RegularStatement getStatement(TableMetadata t);
+
+      protected PreparedOperation(Session session, TableMetadata t) {
+        table = t;
+        preparedStmt = session.prepare(getStatement(table));
       }
 
-      private static TableMetadata getTable(Cluster cluster, String keyspace, String table) {
-        return cluster.getMetadata().getKeyspaces().stream()
-            .filter(k -> k.getName().equals(keyspace))
-            .findAny()
-            .get()
-            .getTable(table);
-      }
-
-      protected abstract PreparedStatement prepare(Session s, TableMetadata t);
-
-      protected PreparedOperation(Cluster cluster, Session session, String keyspaceName, String tableName) {
-        table = getTable(cluster, keyspaceName, tableName);
-        preparedStmt = prepare(session, table);
-      }
-
-      public BoundStatement bind(Change c, ConsistencyLevel cl) {
+      public Statement getStatement(Change c, ConsistencyLevel cl) {
         BoundStatement stmt = preparedStmt.bind();
         stmt.setLong(TIMESTAMP_MARKER_NAME, timeuuidToTimestamp(c.getTime()));
         bindInternal(stmt, c);
@@ -98,8 +106,7 @@ public class Main {
       }
 
       protected void bindPrimaryKeyColumns(BoundStatement stmt, Change c) {
-        Set<String> primaryColumns = table.getPrimaryKey().stream()
-            .map(ColumnMetadata::getName)
+        Set<String> primaryColumns = table.getPrimaryKey().stream().map(ColumnMetadata::getName)
             .collect(Collectors.toSet());
         for (Definition d : c.row.getColumnDefinitions()) {
           if (!d.getName().startsWith("cdc$")) {
@@ -111,8 +118,7 @@ public class Main {
       }
 
       protected void bindPartitionKeyColumns(BoundStatement stmt, Change c) {
-        Set<String> partitionColumns = table.getPartitionKey().stream()
-            .map(ColumnMetadata::getName)
+        Set<String> partitionColumns = table.getPartitionKey().stream().map(ColumnMetadata::getName)
             .collect(Collectors.toSet());
         for (Definition d : c.row.getColumnDefinitions()) {
           if (!d.getName().startsWith("cdc$")) {
@@ -126,9 +132,80 @@ public class Main {
       protected abstract void bindInternal(BoundStatement stmt, Change c);
     }
 
+    private static class UnpreparedUpdateOp implements Operation {
+      private final TableMetadata table;
+
+      public UnpreparedUpdateOp(TableMetadata t) {
+        table = t;
+      }
+
+      @Override
+      public Statement getStatement(Change change, ConsistencyLevel cl) {
+        Update builder = QueryBuilder.update(table);
+        Set<ColumnMetadata> primaryColumns = new HashSet<>(table.getPrimaryKey());
+        table.getColumns().stream().forEach(c -> {
+          TypeCodec<Object> codec = CodecRegistry.DEFAULT_INSTANCE.codecFor(c.getType());
+          if (primaryColumns.contains(c)) {
+            builder.where(
+                eq(c.getName(), change.row.get(c.getName(), codec)));
+          } else {
+            Assignment op = null;
+            if (c.getType().isCollection() && !c.getType().isFrozen() && !change.isDeleted(c.getName())) {
+              DataType innerType = c.getType().getTypeArguments().get(0);
+              TypeToken<Object> type = CodecRegistry.DEFAULT_INSTANCE.codecFor(innerType).getJavaType();
+              TypeToken<Object> type2 = null;
+              if (c.getType().getTypeArguments().size() > 1) {
+                type2 = CodecRegistry.DEFAULT_INSTANCE.codecFor(c.getType().getTypeArguments().get(1 )).getJavaType();
+              }
+              String deletedElementsColumnName = "cdc$deleted_elements_" + c.getName();
+              if (!change.row.isNull(deletedElementsColumnName)) {
+                if (c.getType().getName() == DataType.Name.SET) {
+                  op = removeAll(c.getName(), change.row.getSet(deletedElementsColumnName, type));
+                } else if (c.getType().getName() == DataType.Name.MAP) {
+                  op = removeAll(c.getName(), change.row.getSet(deletedElementsColumnName, type));
+                } else if (c.getType().getName() == DataType.Name.LIST) {
+                  for (UUID key : change.row.getSet(deletedElementsColumnName, UUID.class)) {
+                    builder.with(new ListSetIdxTimeUUIDAssignment(c.getName(), key, null));
+                  }
+                  return;
+                } else {
+                  throw new IllegalStateException();
+                }
+              } else {
+                if (c.getType().getName() == DataType.Name.SET) {
+                  op = addAll(c.getName(), change.row.getSet(c.getName(), type));
+                } else if (c.getType().getName() == DataType.Name.MAP) {
+                  op = putAll(c.getName(), change.row.getMap(c.getName(), type, type2));
+                } else if (c.getType().getName() == DataType.Name.LIST) {
+                  for (Entry<UUID, Object> e : change.row.getMap(c.getName(), TypeToken.of(UUID.class), type).entrySet()) {
+                    builder.with(new ListSetIdxTimeUUIDAssignment(c.getName(), e.getKey(), e.getValue()));
+                  }
+                  return;
+                } else {
+                  throw new IllegalStateException();
+                }
+              }
+            }
+            if (op == null) {
+              op = set(c.getName(), change.row.get(c.getName(), codec));
+            }
+            builder.with(op);
+          }
+        });
+        Integer ttl = change.getTTL();
+        if (ttl != null) {
+          builder.using(timestamp(timeuuidToTimestamp(change.getTime()))).and(ttl(ttl));
+        } else {
+          builder.using(timestamp(timeuuidToTimestamp(change.getTime())));
+        }
+        return builder;
+      }
+
+    }
+
     private static class UpdateOp extends PreparedOperation {
 
-      protected PreparedStatement prepare(Session session, TableMetadata t) {
+      protected RegularStatement getStatement(TableMetadata t) {
         Update builder = QueryBuilder.update(t);
         Set<ColumnMetadata> primaryColumns = new HashSet<>(t.getPrimaryKey());
         t.getColumns().stream().forEach(c -> {
@@ -138,12 +215,13 @@ public class Main {
             builder.with(set(c.getName(), bindMarker(c.getName())));
           }
         });
+
         builder.using(timestamp(bindMarker(TIMESTAMP_MARKER_NAME))).and(ttl(bindMarker(TTL_MARKER_NAME)));
-        return session.prepare(builder);
+        return builder;
       }
 
-      public UpdateOp(Cluster cluster, Session session, String keyspace, String table) {
-        super(cluster, session, keyspace, table);
+      public UpdateOp(Session session, TableMetadata table) {
+        super(session, table);
       }
 
       @Override
@@ -156,15 +234,15 @@ public class Main {
 
     private static class InsertOp extends PreparedOperation {
 
-      protected PreparedStatement prepare(Session session, TableMetadata t) {
+      protected RegularStatement getStatement(TableMetadata t) {
         Insert builder = QueryBuilder.insertInto(t);
         t.getColumns().stream().forEach(c -> builder.value(c.getName(), bindMarker(c.getName())));
         builder.using(timestamp(bindMarker(TIMESTAMP_MARKER_NAME))).and(ttl(bindMarker(TTL_MARKER_NAME)));
-        return session.prepare(builder);
+        return builder;
       }
 
-      public InsertOp(Cluster cluster, Session session, String keyspace, String table) {
-        super(cluster, session, keyspace, table);
+      public InsertOp(Session session, TableMetadata table) {
+        super(session, table);
       }
 
       @Override
@@ -177,15 +255,15 @@ public class Main {
 
     private static class RowDeleteOp extends PreparedOperation {
 
-      protected PreparedStatement prepare(Session session, TableMetadata t) {
+      protected RegularStatement getStatement(TableMetadata t) {
         Delete builder = QueryBuilder.delete().from(t);
         t.getPrimaryKey().stream().forEach(c -> builder.where(eq(c.getName(), bindMarker(c.getName()))));
         builder.using(timestamp(bindMarker(TIMESTAMP_MARKER_NAME)));
-        return session.prepare(builder);
+        return builder;
       }
 
-      public RowDeleteOp(Cluster cluster, Session session, String keyspace, String table) {
-        super(cluster, session, keyspace, table);
+      public RowDeleteOp(Session session, TableMetadata table) {
+        super(session, table);
       }
 
       @Override
@@ -197,15 +275,15 @@ public class Main {
 
     private static class PartitionDeleteOp extends PreparedOperation {
 
-      protected PreparedStatement prepare(Session session, TableMetadata t) {
+      protected RegularStatement getStatement(TableMetadata t) {
         Delete builder = QueryBuilder.delete().from(t);
         t.getPartitionKey().stream().forEach(c -> builder.where(eq(c.getName(), bindMarker(c.getName()))));
         builder.using(timestamp(bindMarker(TIMESTAMP_MARKER_NAME)));
-        return session.prepare(builder);
+        return builder;
       }
 
-      public PartitionDeleteOp(Cluster cluster, Session session, String keyspace, String table) {
-        super(cluster, session, keyspace, table);
+      public PartitionDeleteOp(Session session, TableMetadata table) {
+        super(session, table);
       }
 
       @Override
@@ -217,27 +295,33 @@ public class Main {
 
     private final Session session;
     private final ConsistencyLevel cl;
-    private final Map<Byte, PreparedOperation> preparedOps = new HashMap<>();
+    private final Map<Byte, Operation> preparedOps = new HashMap<>();
 
     public Consumer(Cluster c, Session s, String keyspace, String table, ConsistencyLevel cl) {
       session = s;
       this.cl = cl;
-      preparedOps.put((byte)1, new UpdateOp(c, s, keyspace, table));
-      preparedOps.put((byte)2, new InsertOp(c, s, keyspace, table));
-      preparedOps.put((byte)3, new RowDeleteOp(c, s, keyspace, table));
-      preparedOps.put((byte)4, new PartitionDeleteOp(c, s, keyspace, table));
+      TableMetadata tableMetadata = c.getMetadata().getKeyspaces().stream().filter(k -> k.getName().equals(keyspace))
+          .findAny().get().getTable(table);
+      preparedOps.put((byte) 1,
+          hasCollection(tableMetadata) ? new UnpreparedUpdateOp(tableMetadata) : new UpdateOp(s, tableMetadata));
+      preparedOps.put((byte) 2, new InsertOp(s, tableMetadata));
+      preparedOps.put((byte) 3, new RowDeleteOp(s, tableMetadata));
+      preparedOps.put((byte) 4, new PartitionDeleteOp(s, tableMetadata));
+    }
+
+    private static boolean hasCollection(TableMetadata t) {
+      return t.getColumns().stream().anyMatch(c -> c.getType().isCollection() && !c.getType().isFrozen());
     }
 
     @Override
     public CompletableFuture<Void> consume(Change change) {
-      return FutureUtils.convert(session.executeAsync(preparedOps.get(change.getOperation()).bind(change, cl)));
+      return FutureUtils.convert(session.executeAsync(preparedOps.get(change.getOperation()).getStatement(change, cl)));
     }
 
   }
 
-  private static void replicateChanges(
-      String source, String destination, String keyspace, String table, ConsistencyLevel cl)
-          throws InterruptedException, ExecutionException {
+  private static void replicateChanges(String source, String destination, String keyspace, String table,
+      ConsistencyLevel cl) throws InterruptedException, ExecutionException {
     Signal.handle(new Signal("INT"), signal -> System.exit(0));
     try (Cluster sCluster = Cluster.builder().addContactPoint(source).build();
         Session sSession = sCluster.connect();
@@ -256,8 +340,7 @@ public class Main {
     parser.addArgument("-t", "--table").help("Table name");
     parser.addArgument("-s", "--source").help("Address of a node in source cluster");
     parser.addArgument("-d", "--destination").help("Address of a node in destination cluster");
-    parser.addArgument("-cl", "--consistency-level")
-        .setDefault("quorum")
+    parser.addArgument("-cl", "--consistency-level").setDefault("quorum")
         .help("Consistency level of writes. QUORUM by default");
 
     try {
